59dcc5ef800204a056a6caa367e5bd50e67efbd0
Aspirin <aspirin2d@outlook.com>
2015-02-16 17:56:01 +0800

    add a project file for lime

--- src/CharacterView.hx
+++ src/CharacterView.hx
@@ -3,9 +3,9 @@ package;
 import openfl.display.Sprite;
 
 class CharacterView extends Sprite {
-  public function new()
-  {
-    super();
-    trace("Hello World");
-  }
+    public function new()
+    {
+        super();
+        trace("Hello World");
+    }
 }
