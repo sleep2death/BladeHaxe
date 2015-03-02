0730ec29cb7430ff05ae6f69087d1f7edde4872a
Aspirin <aspirin2d@outlook.com>
2015-03-02 08:18:01 +0800

    add the geo parser

--- src/CharacterView.hx
+++ src/CharacterView.hx
@@ -2,28 +2,58 @@ package;
 
 import openfl.display.Sprite;
 import openfl.net.URLRequest;
+import openfl.events.Event;
 
 import away3d.library.Asset3DLibrary;
 import away3d.library.Asset3DLibraryBundle;
 import away3d.loaders.parsers.ImageParser;
 import away3d.loaders.misc.AssetLoaderToken;
 import away3d.events.Asset3DEvent;
+import away3d.core.base.*;
+import away3d.entities.*;
+import away3d.materials.*;
 
-class CharacterView extends Sprite {
+import hommer.library.*;
+import hommer.core.geometries.*;
+
+class CharacterView extends ViewerBase {
     public function new()
     {
         super();
-        trace("Hello World");
-
-        var libBundle = Asset3DLibrary.getBundle("MyBundle");
-        libBundle.load(new URLRequest("../../../assets/image.jpg"), null, null, new ImageParser());
-        libBundle.addEventListener(Asset3DEvent.ASSET_COMPLETE, onComplete);
+        loadCharacterMesh();
     }
 
-    private function onComplete(evt : Asset3DEvent) : Void
+    private var player : PlayerGeometry;
+    public function loadCharacterMesh() : Void
     {
-        trace("Loaded:" + evt.asset.name + " in " + evt.asset.assetNamespace + " -> " + evt.asset.assetType);
-        var libBundle = Asset3DLibrary.getBundle("MyBundle");
+        player = new PlayerGeometry();
+        player.getSubMeshGroup(["arm_0", "body_0", "belt_0","boot_0", "hair_0", "hand_0", "head_0", "leg_0", "thigh_0"]);
+
+        var mesh : Mesh = new Mesh(player, new ColorMaterial(0xFF9900));
+        _view.scene.addChild(mesh);
+        //var libPlayer : PlayerLibrary = PlayerLibrary.getInstance();
+        //var pma : PlayerSubMeshAsset = libPlayer.getSubMesh("arm_0");
+        //libPlayer.getMesh("arm_0");
+        //libPlayer.getMesh("body_0");
+        //libPlayer.getMesh("belt_0");
+        //libPlayer.getMesh("boot_0");
+        //libPlayer.getMesh("hair_0");
+        //libPlayer.getMesh("hand_0");
+        //libPlayer.getMesh("head_0");
+        //libPlayer.getMesh("leg_0");
+        //libPlayer.getMesh("thigh_0");
+        //libBundle.addEventListener(Asset3DEvent.Asset_COMPLETE, onMeshComplete)
+        //libBundle.load(new URLRequest(PLAYER_MESH_URL), );
+    }
+
+    private var _count : UInt = 0;
+    private override function _onEnterFrame(e:Event):Void
+    {
+        _view.render();
+        if(_count == 300) {
+            player.getSubMeshGroup(["arm_0", "body_0", "belt_0","boot_0", "hair_0", "hand_0", "head_0", "leg_0", "skirt_1"]);
+        }
+        _count++;
     }
 
 }
